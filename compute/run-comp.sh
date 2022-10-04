6b4a07a00afe3868d7ed35abc2453e056776d65f
chennyc <yuanchenincn@gmail.com>
2022-10-04 06:21:01 -0400

    update script

--- compute/run-comp.sh
+++ compute/run-comp.sh
@@ -5,24 +5,6 @@ CMULT="./add_mal $1 runtime-config-$2 private-$1.pem 1 1 fakeinput out "
 MULTSIZE=(1 10 100 1000 10000 100000)
 MULTREP=(1000 1000 1000 100 100 10)
 
-
-CBITOP="./bitop_mal $1 runtime-config-$2 private-$1.pem 1 1 fakeinput out "
-
-for i in {0..5}
-do
-    for k in {1..2}
-    do
-        echo "COMMAND: $CBITOP ${MULTSIZE[$i]} ${MULTREP[$i]}"
-        eval "$CBITOP ${MULTSIZE[$i]} ${MULTREP[$i]}"
-        sleep ${SLEEPTIME[$1]}
-		if [ "$1" = 1 ]
-		then
-		    touch flag.txt
-		fi
-   done
-done
-
-
 for i in {0..5}
 do
     for k in {1..2}
@@ -69,4 +51,19 @@ do
    done
 done
 
+CBITOP="./bitop_mal $1 runtime-config-$2 private-$1.pem 1 1 fakeinput out "
+
+for i in {0..5}
+do
+    for k in {1..2}
+    do
+        echo "COMMAND: $CBITOP ${MULTSIZE[$i]} ${MULTREP[$i]}"
+        eval "$CBITOP ${MULTSIZE[$i]} ${MULTREP[$i]}"
+        sleep ${SLEEPTIME[$1]}
+		if [ "$1" = 1 ]
+		then
+		    touch flag.txt
+		fi
+   done
+done
 
